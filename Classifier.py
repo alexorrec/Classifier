1de8227f6cb542c006968137d813833ea73bc354
Alessandro <alexorrec@gmail.com>
2024-07-27 00:17:23 +0200

    PRNU - SPN - XL files

--- Classifier.py
+++ Classifier.py
@@ -5,6 +5,9 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import pathlib
+from sklearn.metrics import confusion_matrix
+import itertools
+import matplotlib.pyplot as plt
 
 
 class Tester:
@@ -160,3 +163,47 @@ class Tester:
 
     def export_model(self):
         self.model.save(self.__name__ + '.h5')
+
+    def confusion_matrix(self, tensors):
+        predictions = self.model.predict(
+            x=tensors,
+            batch_size=10,
+            verbose=0
+        )
+        rounded_predictions = np.argmax(predictions, axis=1)
+
+        return confusion_matrix(y_true=[0, 1], y_pred=rounded_predictions)
+
+    def plot_confusion_matrix(self, cm, classes,
+                              normalize=False,
+                              title='Confusion matrix',
+                              cmap=plt.cm.Blues):
+        """
+        This function prints and plots the confusion matrix.
+        Normalization can be applied by setting `normalize=True`.
+        """
+        plt.imshow(cm, interpolation='nearest', cmap=cmap)
+        plt.title(title)
+        plt.colorbar()
+        tick_marks = np.arange(len(classes))
+        plt.xticks(tick_marks, classes, rotation=45)
+        plt.yticks(tick_marks, classes)
+
+        if normalize:
+            cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+            print("Normalized confusion matrix")
+        else:
+            print('Confusion matrix, without normalization')
+
+        print(cm)
+
+        thresh = cm.max() / 2.
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            plt.text(j, i, cm[i, j],
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+        plt.tight_layout()
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+        plt.imsave('confusion_matrix.png')
